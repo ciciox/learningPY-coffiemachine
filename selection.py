cf51f79daa59c68d3098d457e5d6533a3b61a787
giuseppe <giuseppe.faro88gmail.com>
2017-05-31 20:31:50 +0100

    new

--- selection.py
+++ selection.py
@@ -1,3 +1,4 @@
+confirm = ('')
 
 def Selection():
     print('Press:')
@@ -19,22 +20,31 @@ def Selection():
     else:
         print ("Your selection hasn't been recognise ")
         print('Please make again your choose')
+    ## manage unexected value return to the begin of IF
 
-    print('You have selected ', sel)
+    print('You have selected ',sel)
     print('Please press "Y" to confirm')
-
     confirm = input()
+    print( 'is 1 ',confirm )
+    #return confirm
+    ### after the return the bariable is not worling
+
+    print( 'is 2',confirm )
     #restart point
 
-def ConfirmSel():
-    if confirm == ('y'):
-        print('Your coffie is in preparation')
-        time.sleep(1)
-        print('Please Wait')
-    elif confirm == ('Y'):
+
+def ConfirmSel(): ## this function is not working ## is not getting the Return
+    #from selection import Selection(confirm)
+
+    import time
+    print( 'is 3',confirm )
+    if 'y' in confirm:
+    #if confirm == yes:
+    #if confirm == 'y' or 'Y':
         print('Your coffie is in preparation')
         time.sleep(1)
         print('Please Wait')
     else:
         print ("Your selection hasn't been recognise ")
         print('Please make again your choose')
+        exit()

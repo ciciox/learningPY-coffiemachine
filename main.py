bbf13e380bfe49828eefc4e708eb78d60e572361
giuseppe <giuseppe.faro88gmail.com>
2017-05-31 16:28:07 +0100

    changed stuff

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-print('Welcome please select your choise')
+import time
+print('Welcome please select your choice')
+time.sleep(2)
 print('Press:')
+time.sleep(1)
 print('1. For Espresso')
 print('2. For Double Espresso')
 print('3. For Americano')
 print('4. For Cappuccino')
+#### restart
 selection = input (int())
+while selection == 1 or 2 or 3 or 4:
+    print ('true')
+'''
+if selection != 1 or 2 or 3 or 4:
+    print ("Your selection hasn't been recognise ")
+    print('Please make again your choose')
+    #### restart the IF
+elif selection = 1:
+    print('You have selected ')
+    print('Please press "Y" to confirm')
+    ##
+    print('Please press "Y" to confirm')
+'''
+
+'''
+elif selection = 2:
+
+elif selection = 3:
+
+else selection = 4:
+
+
+'''
+s
+s

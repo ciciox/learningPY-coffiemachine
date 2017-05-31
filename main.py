46c725563b947cedb1cdca30e25beb370d7b9e89
giuseppe <giuseppe.faro88gmail.com>
2017-05-31 19:26:49 +0100

    new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
-from selection import
+from selection import selectcoffie, confirmsel
+
 print('Welcome please select your choice')
 #time.sleep(1)
 print('Press:')
@@ -9,43 +10,6 @@ print('2. For Double Espresso')
 print('3. For Americano')
 print('4. For Cappuccino')
 #### restart point
+selectcoffie()
 
-'''
-this part works but will be migrate to became a function '''
-'''
-
-selection = int(input ())
-
-if selection == 1:
-    sel = "Espresso"
-elif selection == 2:
-    sel = "Double Espresso"
-elif selection == 3:
-    sel = "Americano"
-elif selection == 4:
-    sel = "Cappuccino"
-else:
-    print ("Your selection hasn't been recognise ")
-    print('Please make again your choose')
-
-
-print('You have selected ', sel)
-print('Please press "Y" to confirm')
-
-confirm = input()
-# restart point
-'''
-
-if confirm == ('y'):
-        #or 'Y':
-    print('Your coffie is in preparation')
-    time.sleep(1)
-    print('Please Wait')
-elif confirm == ('Y'):
-    print('Your coffie is in preparation')
-    time.sleep(1)
-    print('Please Wait')
-else:
-    print ("Your selection hasn't been recognise ")
-    print('Please make again your choose')
-
+confirmsel()
--- selection.py
+++ selection.py
@@ -1,5 +1,5 @@
 
-def selectcoffie()
+def selectcoffie():
     selection = int(input ())
 
     if selection == 1:
@@ -20,3 +20,16 @@ def selectcoffie()
 
     confirm = input()
     #restart point
+
+def confirmsel():
+    if confirm == ('y'):
+        print('Your coffie is in preparation')
+        time.sleep(1)
+        print('Please Wait')
+    elif confirm == ('Y'):
+        print('Your coffie is in preparation')
+        time.sleep(1)
+        print('Please Wait')
+    else:
+        print ("Your selection hasn't been recognise ")
+        print('Please make again your choose')

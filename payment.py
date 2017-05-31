e85851bbfab88ee1016dfc0698c233c6080b12d0
ciciox <giuseppe.faro88@gmail.com>
2017-05-31 23:45:02 +0100

    new

--- payment.py
+++ payment.py
@@ -1,5 +1,20 @@
+
 def Payment():
+    print('The cost for''is 1£')
+    print('Please insert coin')
+    cash = float(input())
+    ## payment is not working need to add the count to the main cash variable
+    while cash < 1:
+        print('The total is', cash,'£')
+        print('You need to add more money')
+        cash = float(input() + cash)
+        return (cash)
+    else:
+        print('caffe')
+
     return ()
 
 def Change():
     return ()
+
+Payment()
--- selection.py
+++ selection.py
@@ -1,12 +1,11 @@
 
 def ConfirmSel():
-
     confirm = Selection()
-
-    value = ['y','yes','Y','YES']
     import time
     print('Please press "Y" to confirm')
     confirm = input()
+    value = ['y', 'yes', 'Y', 'YES']
+
     while confirm is not value:
 
         if confirm in value:
@@ -23,7 +22,7 @@ def ConfirmSel():
 def Selection():
     import time
     print('Press:')
-    #time.sleep(1)
+    time.sleep(1)
     mylist = ['1. For Espresso','2. For Double Espresso','3. For Americano','4. For Cappuccino']
     print("\n".join(mylist))
     selection = int(input ())
@@ -39,6 +38,41 @@ def Selection():
     else:
         print ("Your selection hasn't been recognise ")
         print('Please make again your choose')
-    ## manage unexected value return to the begin of IF
-    print('You have selected ',sel)
+    print('You have selected',sel)
 
+'''
+    
+    ## manage unexected value if different from the selection return to the begin of IF
+
+    valuec = ['1', '2', '3', '4']
+    except is not valuec:
+        try:
+            print('eccezione')
+        except:
+            break
+
+'''
+'''
+def Selection():
+    import time
+    print('Press:')
+    time.sleep(1)
+    mylist = ['1. For Espresso','2. For Double Espresso','3. For Americano','4. For Cappuccino']
+    print("\n".join(mylist))
+    selection = int(input ())
+    while selection is not mylist:
+        if selection == 1:
+            sel = "Espresso"
+        elif selection == 2:
+            sel = "Double Espresso"
+        elif selection == 3:
+            sel = "Americano"
+        elif selection == 4:
+            sel = "Cappuccino"
+        else:
+            print ("Your selection hasn't been recognise ")
+            print('Please make again your choose')
+            selection = int(input())
+        print('You have selected',sel)
+
+'''

1a003014431c53ee4ee56521f4f4dd01eb46d841
giuseppe <giuseppe.faro@gmail.com>
2017-06-01 13:39:09 +0100

    new

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 from selection import Main
-
+from payment import Payment, Change
 print('Welcome please select your choice')
 
-time.sleep(1)
-
+#time.sleep(1)
 
 Main()
-time.sleep(1)
+#time.sleep(1)
 print ('Your coffie now is Ready')
 print (credit)
+
+#balance = Payment()
+#print(balance)

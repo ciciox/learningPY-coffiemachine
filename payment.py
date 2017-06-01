39fffe7792932dbd226ce14b1bbe5f78d7b69f03
giuseppe <giuseppe.faro@gmail.com>
2017-06-01 13:40:04 +0100

    new

--- payment.py
+++ payment.py
@@ -1,3 +1,16 @@
+'''  # this is working fine
+def Payment():
+    global credit
+    print('The cost for''is 1£')
+    print('Please insert coin')
+    credit = float(input())
+    while credit < 1:
+        print('Your credit now is', credit,'£')
+        print('You still need to add other',-credit+1,'cent')
+        newcredit = float(input())
+        credit = newcredit + credit
+    #return credit
+'''
 
 def Payment():
     global credit
@@ -10,10 +23,10 @@ def Payment():
         newcredit = float(input())
         credit = newcredit + credit
     #return credit
-
 # uncomment for debugging
-Payment()
-print(credit)
+#Payment()
+
+#print(credit)
 #print (credit)
 
 

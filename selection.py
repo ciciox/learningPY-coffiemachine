64a427c1971c2d3ae9d62ec2167fa542490aaa86
Giuseppe Faro <giuseppe.faro88@gmail.com>
2017-06-02 23:03:33 +0100

    none

--- selection.py
+++ selection.py
@@ -40,19 +40,31 @@ def Selection():
     time.sleep(1)
     mylist = ['1. For Espresso','2. For Double Espresso','3. For Americano','4. For Cappuccino']
     print("\n".join(mylist))
-    selection = int(input ())
-    global sel
-    if selection == 1:
-        sel = "Espresso"
-    elif selection == 2:
-        sel = "Double Espresso"
-    elif selection == 3:
-        sel = "Americano"
-    elif selection == 4:
-        sel = "Cappuccino"
-    else:
-        print ("Your selection hasn't been recognise ")
-        print('Please make again your choose')
+    selection = 0
+    while selection != mylist:
+        selection = int(input ())
+        global sel
+        if selection == 1:
+            sel = "Espresso"
+            break
+        elif selection == 2:
+            sel = "Double Espresso"
+            break
+        elif selection == 3:
+            sel = "Americano"
+            break
+        elif selection == 4:
+            sel = "Cappuccino"
+            break
+        else:
+            try:
+                print("Your selection hasn't been recognise ")
+                print('Please make again your choose:')
+                print("\n".join(mylist))
+                continue
+            #### this part of the except is not working
+            except ValueError:
+                print('value error 2')
     print('You have selected',sel)
 
 
